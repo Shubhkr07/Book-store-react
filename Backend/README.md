2fe7608fe6aa780c27f396d11a872d2dd88f9b19
Shubhkr07 <shubh37kumar@gmail.com>
2023-08-18 19:31:29 +0530

    addded fronted code

---
+++ Backend/README.md
@@ -0,0 +1,10 @@
+# Book Store Application
+
+### This is a mern stack web application for an online book store.
+### Its features are
+- Filter books on basis of categories.
+- Place order for books.
+- Responsive and Interactive user Interface.
+- Manage book catalog,add new books,
+- updating existing book
+details and removing books.
---
+++ Frontend/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from "react-router-dom";
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   </BrowserRouter>,
+//   document.getElementById("root")
+// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+
+  </BrowserRouter>
+);
