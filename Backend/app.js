fe56142cbb679106b38ab9a7ee47fd7e70be6b42
Shubhkr07 <shubh37kumar@gmail.com>
2023-08-18 19:13:33 +0530

    cleared unwanted files

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const  mongoose = require('mongoose');  
+const mongoose = require('mongoose');  
 const router = require("./routes/book-routes");
 const cors = require('cors');
 const app = express();
